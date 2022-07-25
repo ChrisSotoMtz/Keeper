0fc775989dde14facb666495f5a2c5dcc024d0c6
Christian Soto <71237998+ChrisSotoMtz@users.noreply.github.com>
2022-07-24 21:41:24 -0500

    quickFix 3

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/globals.css";
 
 import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from "recoil";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  useEffect(() => {
+    import ("bootstrap/dist/css/bootstrap.min.css");
+    import ("react-bootstrap/dist/react-bootstrap.min.js");
+    import ("../styles/globals.css");
+
+  }, []);
   return (
     <SessionProvider session={session}>
       <RecoilRoot>
